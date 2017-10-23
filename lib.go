f9a45fcc830f1bfe013d57b8058fbdc168f46dce
Rob Muhlestein <rob@skilstak.com>
2017-10-22 20:59:34 -0400

    save

--- lib.go
+++ lib.go
@@ -1,4 +1,20 @@
 package storyeng
 
 type Part map[string]interface{}
-type Parts map[string]Part
+type partst map[string]Part
+
+type Event struct {}
+type InputEvent struct {
+	Event
+	Line string
+	Lower string
+	Yes bool
+	No bool
+}
+
+var Parts = partst{}
+var Data = map[string]interface{}{}
+
+func Start() {
+	println("starting")
+}
